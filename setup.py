cd956f34b4bb6af4f7d6b810d54af544790fa32e
Alex Spaeth <atspaeth@ucsc.edu>
2024-05-22 18:15:05 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="SpikeData",
+    version="0.1.0",
+    description="Data structure and analysis for spike data",
+    py_modules=["spikedata"],
+    requires=[
+        "numpy",
+        "scipy(>=1.10.0)",
+        "powerlaw",
+    ],
+)
