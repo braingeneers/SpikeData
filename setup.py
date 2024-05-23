3843e1787804b089ca28bf414f8c77e10a24b856
Alex Spaeth <atspaeth@ucsc.edu>
2024-05-23 14:10:50 -0700

    Fix pip install requires

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="Data structure and analysis for spike data",
     py_modules=["spikedata"],
-    requires=[
+    install_requires=[
         "numpy",
         "scipy(>=1.10.0)",
         "powerlaw",
