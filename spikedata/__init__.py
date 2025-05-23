3d3533a00b141ad06e1ec984c49204bdf82e1f9f
Alex Spaeth <atspaeth@ucsc.edu>
2025-05-22 17:57:30 -0700

    Use hatch-vcs to generate version

--- spikedata/__init__.py
+++ spikedata/__init__.py
@@ -1,5 +1,6 @@
 # Import everything in __all__ in spikedata.py, which for some reason loads the module
 # object itself into global scope, so delete it afterwards.
 from .spikedata import *  # noqa F401
+from ._version import version as __version__
 
 del spikedata  # noqa F821
